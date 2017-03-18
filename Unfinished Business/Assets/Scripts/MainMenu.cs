fe00d3315a84e03102e3d1a23e526bc654b4a567
alecbiela <alec.bielanos@gmail.com>
2017-03-18 17:12:18 -0400

    Added Main Menu, Scene Loader, Changed Prefabs
    Added a main menu scene that should be the launch scene whenever we
    build standalone.  This currently has 4 buttons, to load the 3 demo
    scenes and one to get to the options screen (currently blank).
    
    Updated the GameManager and Player to be prefabs in the foyer scene.
    **IMPORTANT: When making changes to the scripts make sure to hit "apply"
    to the prefabs so the changes persist across scenes.
    
    Lastly, ensure that objects do not have 2 colliders on them, as the
    object viewer will only turn 1 off and the second collider will push the
    player around when viewing.

---
+++ Unfinished Business/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private GameObject mainScreen;
+    private GameObject optionsScreen;
+
+    //initialization
+    void Awake()
+    {
+        mainScreen = GameObject.Find("MainUI");
+        optionsScreen = GameObject.Find("OptionsUI");
+        optionsScreen.SetActive(false);
+    }
+
+    //handles scene change button presses
+    public void StartScene(string sceneName)
+    {
+        //closes all open scenes and loads the new scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
+    //handles pressing of the 'options' button
+    public void ToggleOptions()
+    {
+        //hide/show the options screen
+        optionsScreen.SetActive(!optionsScreen.activeInHierarchy);
+
+        //hide/show the main screen
+        mainScreen.SetActive(!mainScreen.activeInHierarchy);
+    }
+}
